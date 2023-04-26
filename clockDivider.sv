d2052f844450b19a7471c847ce98197bb76f9c13
VictorClements <104516656+VictorClements@users.noreply.github.com>
2023-04-26 10:05:41 -0700

    Create clockDivider.sv

---
+++ clockDivider.sv
@@ -0,0 +1,13 @@
+module clockDivider #(paramter WIDTH = 32, FREQENCY_IN = 50000000, FREQUENCY_OUT = 1, MULTIPLIER = ((2**WIDTH)*FREQUENCY_OUT - 1)/FREQUENCY_IN + 1)
+                     (input  logic clk, reset,
+                      output logic slowClk);
+
+  logic [WIDTH-1:0] count;
+
+  always_ff @(posedge clk, posedge reset)
+    if (reset)  count <= '0;
+    else        count <= count + MULTIPLIER;
+  
+  assign slowClk = count[WIDTH-1];
+
+endmodule
