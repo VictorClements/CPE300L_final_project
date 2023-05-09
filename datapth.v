06021781c5e58298b7551b981735deda651c6a3e
kimmic0 <132402682+kimmic0@users.noreply.github.com>
2023-05-09 16:46:13 -0700

    Update datapth.v

--- datapth.v
+++ datapth.v
@@ -20,7 +20,7 @@ wire [31:0] pcnext, pcnextbr, pcplus4, pcbranch, nextpcbranch; //TODO: new wire 
 wire [31:0] signimm, signimmsh, muxout4, signex4, r1, r2;
 wire [31:0] srca, srcb;
 wire [31:0] result, operout, muxreg2, muxnext, lbmuxout, finalout;
-wire [31:0] wdmuxout, jalmuxout, muxpcout;
+wire [31:0] jalmuxout, muxpcout;
 
 // next PC logic. Instantiates PC and adders
 flopr #(32) pcreg(clk, reset, pcnext, pc);
@@ -35,20 +35,20 @@ mux2 #(32) muxthree(pcnextbr, srca, jr, nextpcbranch); //TODO: new mux for jr
 
 // register file logic; instatiates RF
 regfile rf(clk, regwrite, instr[25:21],
-instr[20:16], writereg, wdmuxout, srca, writedata); 
+instr[20:16], writereg, finalout, srca, writedata); //TODO: altered for other instructions
 multiplyanddivider mad(srca, srcb, mult, operout); //TODO: picks between multiply and divide
 mux2 #(32) regsel(r1, r2, mfhi, muxreg2); //TODO: selects which register to use 
 
 
 //instatiates the number of multiplexers
-mux2 #(5) wrmux(instr[20:16], muxpcout, regdst, writereg); //TODO: edited
+mux2 #(5) wrmux(instr[20:16], muxpcout, regdst, writereg); //TODO: edited for jal instruction
 mux2 #(5) wrpre(instr[15:11], pcplus4[31], jal, muxpcout ); //TODO: new mux for jal
 //mux2 #(32) resmux(aluout, jalmuxout, memtoreg, result); //TODO: edited
 mux4 lbmux4(readdata[7:0], readdata[15:8], readdata[23:16], readdata[31:24], {aluout[31:2], 2'b00}, muxout4); //TODO: lb instruction
 signext se2(muxout4, signex4); //TODO: lb instruction
 mux2 lbmux(readdata, signex4, lb, lbmuxout); //TODO: readdata or lb
 mux2 lbnextmux(lbmuxout, muxreg2, lb, muxnext); //TODO: decides between lb and register
-mux2 finalmux(muxnext, aluout, alures, finalout); //TODO: FINAL mux
+mux2 finalmux(aluout, muxnext, alures, finalout); //TODO: FINAL mux
 signext se(instr[15:0], signimm); ///TODO: lb instruction
 
 // ALU logic: defines inputs and instatiates Alu
